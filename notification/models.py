742ae3f702e93a05013ebbba5117f1adf43f457c
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-06 16:13:09 +0900

    modifed model

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NotificationQueue(models.Model):
+  device_token = models.CharField(max_length = 255)
+  title = models.CharField(max_length = 255)
+  message = models.CharField(max_length = 255)
