526fcfc592431546074833e11e5921bcbfbcf7d1
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-11 15:19:34 +0900

    add new model

--- notification/models.py
+++ notification/models.py
@@ -7,3 +7,19 @@ class NotificationQueue(models.Model):
   device_token = models.CharField(max_length = 255)
   title = models.CharField(max_length = 255)
   message = models.CharField(max_length = 255)
+
+
+class AppleNotificationDevices(models.Model):
+  token = models.CharField(max_length = 255)
+  version = models.CharField(max_length = 255)
+  device = models.CharField(max_length = 255)
+  registered_at = models.DateTimeField()
+  is_disabled = models.BooleanField()
+
+
+class AndroidNotificationDevices(models.Model):
+  token = models.CharField(max_length = 255)
+  version = models.CharField(max_length = 255)
+  device = models.CharField(max_length = 255)
+  registered_at = models.DateTimeField()
+  is_disabled = models.BooleanField()
